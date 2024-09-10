5a9c09631e84f15b80129ac7b802fcb3553e5e22
Klisman Evangelista <klismanevangelista777@gmail.com>
2024-09-10 12:01:28 -0500

    Validaciones modelo plan sin latitud ni longitud

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,14 @@
 class Plan < ApplicationRecord
   belongs_to :user
+
+  DEPARTAMENTS = [
+    'Amazonas', 'Áncash', 'Apurímac', 'Arequipa', 'Ayacucho', 'Cajamarca',
+    'Callao', 'Cusco', 'Huancavelica', 'Huánuco', 'Ica', 'Junín', 'La Libertad',
+    'Lambayeque', 'Lima', 'Loreto', 'Madre de Dios', 'Moquegua', 'Pasco', 'Piura',
+    'Puno', 'San Martín', 'Tacna', 'Tumbes', 'Ucayali'
+  ]
+
+  validates :name, :description, :location, :travelers_quantity, :start_date, :end_date, :start_point, presence: true
+
+  validates :departament, inclusion: { in: DEPARTAMENTS, message: "%{value} no es un departamento válido" }
 end
