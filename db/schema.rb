438dfce251b4a24969e36f842bdce0c39288d3cb
Merly Anco <merly2257@gmail.com>
2024-09-10 17:49:16 -0700

    terminado view login y register

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_10_065641) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_11_003828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,7 +62,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_10_065641) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
-    t.string "birth_date"
+    t.date "birth_date"
     t.string "dni"
     t.string "occupation"
     t.string "phone"
