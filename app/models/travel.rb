b14bf137e8b936653bb012beb23daa9f8cf68cfe
eugehn olivares <olivares.eugenh@gmail.com>
2024-09-13 12:37:33 -0500

    max number of tripmates

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -5,4 +5,19 @@ class Travel < ApplicationRecord
   has_many :reviews
 
   validates :user_id, presence: true, uniqueness: { scope: :plan_id }
+  before_create :check_accepted_travel_max_quantity
+
+  private
+
+  def check_accepted_travel_max_quantity
+    if status == 'aceptado'
+      num_acepted_travels = plan.travels.where(status: 'aceptado').count
+      travelers_quantity = plan.travelers_quantity
+
+      max_quantity_travelers = num_acepted_travels >= travelers_quantity
+
+      errors.add(:base, 'Se ha alcanzado el número máximo de solitudes aceptadas.') if max_quantity_travelers
+      throw :abort if max_quantity_travelers
+    end
+  end
 end
