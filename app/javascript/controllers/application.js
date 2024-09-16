290e3478bc2806ff551d25ca8bff893c63365545
eugehn olivares <olivares.eugenh@gmail.com>
2024-09-16 14:14:44 -0500

    Autocomplete direccion in registration and change in regexp for public address in user profile

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,6 +7,7 @@ import MapController from "./map_controller"
 import NewPlanController from "./new_plan_controller"
 import ShowPlanController from "./show_plan_controller"
 import AllMyPlansController from "./all_my_plans_controller"
+import AutocompleteAddressController from "./autocomplete_address_controller"
 
 const application = Application.start()
 application.register('navbar', NavbarController)
@@ -17,6 +18,7 @@ application.register('map-filter', MapController)
 application.register('new-plan', NewPlanController)
 application.register('show-plan', ShowPlanController)
 application.register('my-plans', AllMyPlansController)
+application.register('autocomplete-address', AutocompleteAddressController)
 
 // Configure Stimulus development experience
 application.debug = false
