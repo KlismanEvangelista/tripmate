06cb20bef0b5f984a46e9fc660fb3c1d34ac5d9f
Klisman Evangelista <klismanevangelista777@gmail.com>
2024-09-17 17:54:46 -0500

    Chat incompleto

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,12 @@
 class MessagesController < ApplicationController
+  def index
+    @messages = Message.includes(:plan).all.group_by(&:plan_id)
+    @plans = Plan.where(id: @messages.keys)
+  end
+
   def new
+    @messages = Message.includes(:plan).all.group_by(&:plan_id)
+    @plans = Plan.where(id: @messages.keys)
     @plan = Plan.find(params[:plan_id])
     @message = Message.new
   end
@@ -10,7 +17,14 @@ class MessagesController < ApplicationController
     @message.plan = @plan
     @message.user = current_user
     if @message.save
-      redirect_to new_plan_message_path(@plan)
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append(
+            :messages, partial: "messages/chanel", locals: {plan: @plan, new_message: @message}
+          )
+        end
+        format.html { redirect_to new_plan_message_path(@plan) }
+      end
     end
   end
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,14 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :plan
+
+  after_create_commit :broadcast_message
+
+  private
+
+  def broadcast_message
+    broadcast_append_to "plan_#{plan.id}_messages",
+                        partial: "messages/chanel",
+                        locals: { plan: plan, new_message: self }
+  end
 end
